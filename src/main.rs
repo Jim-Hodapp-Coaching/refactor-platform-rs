a8fc908d3c2b7650248872aac75a9f8a61b5e7c1
Jim Hodapp <james.hodapp@gmail.com>
2023-11-02 19:23:24 -0500

    Add GPLv3 license

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2023 Refactor Coaching, Inc.
+// Copyright (c) 2023 Jim Hodapp & Caleb Bourg
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
