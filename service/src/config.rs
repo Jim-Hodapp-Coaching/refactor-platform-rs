d007f0cdaa4cb89aa6de437f62507eaf2b35564f
Jim Hodapp <james.hodapp@gmail.com>
2023-11-18 14:24:16 -0600

    Adds two new CLI flags to set the host side TCP interface and port to listen on for incoming client connections. Sets the default port to 4000

--- service/src/config.rs
+++ service/src/config.rs
@@ -12,6 +12,14 @@ pub struct Config {
     )]
     pub database_uri: Option<String>,
 
+    /// The host interface to listen for incoming connections
+    #[arg(short, long, default_value = "127.0.0.1")]
+    pub interface: Option<String>,
+
+    /// The host TCP port to listen for incoming connections
+    #[arg(short, long, default_value_t = 4000)]
+    pub port: u16,
+
     /// Turn on different tracing levels [0 = Warn, 1 = Info, 2 = Debug, 3 = Trace]
     #[arg(short, long, default_value_t = 0)]
     pub trace_level: u8,
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -14,16 +14,16 @@ mod router;
 
 pub async fn init_server(app_state: AppState) -> Result<()> {
     // These will probably come from app_state.config (command line)
-    let host = "127.0.0.1";
-    let port = 3000;
+    let host = app_state.config.interface.as_ref().unwrap();
+    let port = app_state.config.port;
     let server_url = format!("{host}:{port}");
 
-    let addr = SocketAddr::from_str(&server_url).unwrap();
+    let listen_addr = SocketAddr::from_str(&server_url).unwrap();
 
     info!("Server starting... listening for connections on http://{host}:{port}");
 
     // using unwrap() here as the app should panic if the server cannot start
-    Server::bind(&addr)
+    Server::bind(&listen_addr)
         .serve(router::define_routes(app_state).into_make_service())
         .await
         .unwrap();
