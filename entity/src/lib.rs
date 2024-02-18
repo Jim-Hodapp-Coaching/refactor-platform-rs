fd817af3badf08fdb0a005745d71326d8f2109c8
Caleb Bourg <calebbourg2@gmail.com>
2024-02-18 08:47:37 -0500

    add external_id to tables
    Also reverts primary keys to integer values

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -1,8 +1,7 @@
-use uuid::Uuid;
 
 pub mod coaching_relationship;
 pub mod organization;
 pub mod user;
 
 /// A type alias that represents any Entity's id field data type
-pub type Id = Uuid;
+pub type Id = u64;
