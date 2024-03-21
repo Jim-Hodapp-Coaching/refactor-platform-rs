ceda7cf61d7ee07bc13bb1a1805b1e1b5915585a
Jim Hodapp <james.hodapp@gmail.com>
2024-03-21 11:41:34 -0500

    Cleaner, easier to read custom extractor implementation

--- web/src/custom_extractors.rs
+++ web/src/custom_extractors.rs
@@ -6,6 +6,8 @@ use axum::{
 };
 use log::*;
 
+type RejectionType = (StatusCode, &'static str);
+
 pub static X_VERSION: &str = "x-version";
 
 pub struct CheckApiVersion(pub HeaderValue);
@@ -16,7 +18,7 @@ where
     AppState: FromRef<S>,
     S: Send + Sync,
 {
-    type Rejection = (StatusCode, &'static str);
+    type Rejection = RejectionType;
 
     // A custom Extractor that extracts and checks that the API version number
     // provided in the "X-Version" header is equal to the API version specified
@@ -25,18 +27,31 @@ where
     // successfully.
     async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
         let state = AppState::from_ref(state);
+        let version = get_x_version(parts)?;
+        let api_version = HeaderValue::from_str(&state.config.api_version.unwrap_or_default()).ok().unwrap();
 
-        if let Some(version) = parts.headers.get(X_VERSION) {
-            trace!("API version provided by client: {:?}", version);
-            match state.config.api_version == Some(version.to_str().unwrap_or_default().into()) {
-                true => Ok(CheckApiVersion(version.clone())),
-                false => Err((
-                    StatusCode::BAD_REQUEST,
-                    "`X-Version` header is not a valid API version",
-                )),
-            }
-        } else {
-            Err((StatusCode::BAD_REQUEST, "`X-Version` header is missing"))
-        }
+        trace!("API version provided by client: {:?}", version);
+        trace!("API version set in AppState.config.api_version: {:?}", version);
+
+        Ok(is_current_api_version(version, api_version)?)
+    }
+}
+
+fn get_x_version(parts: &mut Parts) -> Result<HeaderValue, RejectionType> {
+    if let Some(version) = parts.headers.get(X_VERSION) {
+        Ok(version.clone())
+    } else {
+        Err((StatusCode::BAD_REQUEST, "`X-Version` header is missing"))
+    }
+}
+
+fn is_current_api_version(version: HeaderValue, api_version: HeaderValue) -> Result<CheckApiVersion, RejectionType> {
+    if version == api_version {
+        Ok(CheckApiVersion(version))
+    } else {
+        Err((
+            StatusCode::BAD_REQUEST,
+            "`X-Version` header is not a valid API version",
+        ))
     }
 }
