795739ff35bf5ec95450937478cf9cec309b2318
Jim Hodapp <james.hodapp@gmail.com>
2024-03-24 18:40:10 -0500

    Remove utoipa_redoc since it's the least attractive UI

--- web/src/router.rs
+++ web/src/router.rs
@@ -12,7 +12,6 @@ use utoipa::{
     Modify, OpenApi,
 };
 use utoipa_rapidoc::RapiDoc;
-use utoipa_redoc::{Redoc, Servable};
 use utoipa_swagger_ui::SwaggerUi;
 
 // This is the global definition of our OpenAPI spec. To be a part
@@ -72,7 +71,6 @@ pub fn define_routes(app_state: AppState) -> Router {
         .merge(protected_routes())
         // FIXME: protect the OpenAPI web UI endpoint we end up choosing to go with
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
-        .merge(Redoc::with_url("/redoc", ApiDoc::openapi()))
         // There is no need to create `RapiDoc::with_openapi` because the OpenApi is served
         // via SwaggerUi instead we only make rapidoc to point to the existing doc.
         .merge(RapiDoc::new("/api-docs/openapi.json").path("/rapidoc"))
