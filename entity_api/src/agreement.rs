3f0a19dcde3f907c3a823b472af15e050d14172e
Caleb Bourg <calebbourg2@gmail.com>
2024-08-01 07:31:43 -0400

    add index, read, update for agreements

---
+++ entity_api/src/agreement.rs
@@ -0,0 +1,167 @@
+use super::error::{EntityApiErrorCode, Error};
+use crate::uuid_parse_str;
+use entity::agreements::{self, ActiveModel, Entity, Model};
+use entity::Id;
+use sea_orm::{
+    entity::prelude::*,
+    ActiveValue::{Set, Unchanged},
+    DatabaseConnection, TryIntoModel,
+};
+use std::collections::HashMap;
+
+use log::*;
+
+pub async fn create(db: &DatabaseConnection, agreement_model: Model) -> Result<Model, Error> {
+    debug!("New Agreement Model to be inserted: {:?}", agreement_model);
+
+    let now = chrono::Utc::now();
+
+    let agreement_active_model: ActiveModel = ActiveModel {
+        coaching_session_id: Set(agreement_model.coaching_session_id),
+        details: Set(agreement_model.details),
+        user_id: Set(agreement_model.user_id),
+        created_at: Set(now.into()),
+        updated_at: Set(now.into()),
+        ..Default::default()
+    };
+
+    Ok(agreement_active_model.save(db).await?.try_into_model()?)
+}
+
+pub async fn update(db: &DatabaseConnection, id: Id, model: Model) -> Result<Model, Error> {
+    let result = Entity::find_by_id(id).one(db).await?;
+
+    match result {
+        Some(agreement) => {
+            debug!("Existing Agreement model to be Updated: {:?}", agreement);
+
+            let active_model: ActiveModel = ActiveModel {
+                id: Unchanged(agreement.id),
+                coaching_session_id: Unchanged(agreement.coaching_session_id),
+                details: Set(model.details),
+                user_id: Unchanged(agreement.user_id),
+                updated_at: Set(chrono::Utc::now().into()),
+                created_at: Unchanged(agreement.created_at),
+            };
+
+            Ok(active_model.update(db).await?.try_into_model()?)
+        }
+        None => {
+            debug!("Agreement with id {} not found", id);
+
+            Err(Error {
+                inner: None,
+                error_code: EntityApiErrorCode::RecordNotFound,
+            })
+        }
+    }
+}
+
+pub async fn find_by(
+    db: &DatabaseConnection,
+    query_params: HashMap<String, String>,
+) -> Result<Vec<Model>, Error> {
+    let mut query = Entity::find();
+
+    for (key, value) in query_params {
+        match key.as_str() {
+            "coaching_session_id" => {
+                let coaching_session_id = uuid_parse_str(&value)?;
+
+                query = query.filter(agreements::Column::CoachingSessionId.eq(coaching_session_id));
+            }
+            _ => {
+                return Err(Error {
+                    inner: None,
+                    error_code: EntityApiErrorCode::InvalidQueryTerm,
+                });
+            }
+        }
+    }
+
+    Ok(query.all(db).await?)
+}
+
+#[cfg(test)]
+// We need to gate seaORM's mock feature behind conditional compilation because
+// the feature removes the Clone trait implementation from seaORM's DatabaseConnection.
+// see https://github.com/SeaQL/sea-orm/issues/830
+#[cfg(feature = "mock")]
+mod tests {
+    use super::*;
+    use entity::{agreements::Model, Id};
+    use sea_orm::{DatabaseBackend, MockDatabase, Transaction};
+
+    #[tokio::test]
+    async fn create_returns_a_new_agreement_model() -> Result<(), Error> {
+        let now = chrono::Utc::now();
+
+        let agreement_model = Model {
+            id: Id::new_v4(),
+            coaching_session_id: Id::new_v4(),
+            details: Some("This is a agreement".to_owned()),
+            user_id: Id::new_v4(),
+            created_at: now.into(),
+            updated_at: now.into(),
+        };
+
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results(vec![vec![agreement_model.clone()]])
+            .into_connection();
+
+        let agreement = create(&db, agreement_model.clone().into()).await?;
+
+        assert_eq!(agreement.id, agreement_model.id);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn update_returns_an_updated_agreement_model() -> Result<(), Error> {
+        let now = chrono::Utc::now();
+
+        let agreement_model = Model {
+            id: Id::new_v4(),
+            coaching_session_id: Id::new_v4(),
+            details: Some("This is a agreement".to_owned()),
+            user_id: Id::new_v4(),
+            created_at: now.into(),
+            updated_at: now.into(),
+        };
+
+        let db = MockDatabase::new(DatabaseBackend::Postgres)
+            .append_query_results(vec![vec![agreement_model.clone()], vec![agreement_model.clone()]])
+            .into_connection();
+
+        let agreement = update(&db, agreement_model.id, agreement_model.clone()).await?;
+
+        assert_eq!(agreement.details, agreement_model.details);
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn find_by_returns_all_agreements_associated_with_coaching_session() -> Result<(), Error> {
+        let db = MockDatabase::new(DatabaseBackend::Postgres).into_connection();
+        let mut query_params = HashMap::new();
+        let coaching_session_id = Id::new_v4();
+
+        query_params.insert(
+            "coaching_session_id".to_owned(),
+            coaching_session_id.to_string(),
+        );
+
+        let _ = find_by(&db, query_params).await;
+
+        assert_eq!(
+            db.into_transaction_log(),
+            [Transaction::from_sql_and_values(
+                DatabaseBackend::Postgres,
+                r#"SELECT "agreements"."id", "agreements"."coaching_session_id", "agreements"."details", "agreements"."user_id", "agreements"."created_at", "agreements"."updated_at" FROM "refactor_platform"."agreements" WHERE "agreements"."coaching_session_id" = $1"#,
+                [coaching_session_id.into()]
+            )]
+        );
+
+        Ok(())
+    }
+}
