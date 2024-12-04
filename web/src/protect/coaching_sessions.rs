6bcebf2a0e9e7b097e70fde0a3c92babb73ae6c4
Caleb Bourg <calebbourg2@gmail.com>
2024-12-04 10:41:22 -0500

    add protect::coaching_relationships::index

---
+++ web/src/protect/coaching_sessions.rs
@@ -0,0 +1,55 @@
+use crate::{extractors::authenticated_user::AuthenticatedUser, AppState};
+use axum::{
+    extract::{Query, Request, State},
+    http::StatusCode,
+    middleware::Next,
+    response::IntoResponse,
+};
+
+use entity::Id;
+use entity_api::coaching_relationship;
+use std::collections::HashMap;
+
+// struct Params {
+//     coaching_relationship_id: Option<Id>,
+// }
+
+/// Checks that coaching relationship record referenced by `coaching_relationship_id`
+/// exists and that the authenticated user is associated with it.
+///  Intended to be given to axum::middleware::from_fn_with_state in the router
+pub(crate) async fn index(
+    State(app_state): State<AppState>,
+    AuthenticatedUser(user): AuthenticatedUser,
+    // Note that we're being explicit that the query hashmap here has values of typ Id
+    // This allows us to deserialize the UUID string to a Uuid automatically
+    // This might come back to bite us if we start allowing other params for endpoints that use this.
+    // We'll have to come back and change things in that case.
+    Query(params): Query<HashMap<String, Id>>,
+    request: Request,
+    next: Next,
+) -> impl IntoResponse {
+    if let Some(coaching_relationship_id) = params.get("coaching_relationship_id") {
+        let coaching_relationship =
+            coaching_relationship::find_by_id(app_state.db_conn_ref(), *coaching_relationship_id)
+                .await
+                .unwrap_or(None);
+        match coaching_relationship {
+            Some(coaching_relationship) => {
+                if coaching_relationship.coach_id == user.id
+                    || coaching_relationship.coachee_id == user.id
+                {
+                    // User has access to coaching relationship
+                    next.run(request).await
+                } else {
+                    // User does not have access to coaching relationship
+                    (StatusCode::UNAUTHORIZED, "UNAUTHORIZED").into_response()
+                }
+            }
+            // coaching relationship with given ID not found
+            None => (StatusCode::NOT_FOUND, "NOT FOUND").into_response(),
+        }
+    } else {
+        // No coaching relationship ID provided
+        (StatusCode::BAD_REQUEST, "BAD REQUEST").into_response()
+    }
+}
